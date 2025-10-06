b3b42ad175e7ffbb8674de7e38718f128674fcae
AlBaraa-1 <666645@gmail.com>
2025-10-06 22:51:57 +0400

    Add image input/output example using OpenCV

---
+++ baby_steps/01_io_img.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+
+# get the path of the image
+img_path = os.path.join('.', 'test.png')
+
+# read the image
+img = cv2.imread(img_path)
+
+# save the image to a new file - optional
+cv2.imwrite('output.png', img)
+
+# display the image in a window
+cv2.imshow('image', img)
+cv2.waitKey(0)
