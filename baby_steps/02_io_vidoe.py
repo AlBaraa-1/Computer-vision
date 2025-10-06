89816d9be0494d28557d277e0b9e186b57c49b24
AlBaraa-1 <666645@gmail.com>
2025-10-06 23:03:48 +0400

    Add video playback functionality using OpenCV

---
+++ baby_steps/02_io_vidoe.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+
+# get the path of the video
+video_path = os.path.join('.', 'test.mp4')
+
+# read the video 
+video = cv2.VideoCapture(video_path)
+
+# display the video in a window
+ret = True # ret is boolean to know that we have frames
+while ret:
+    ret, frame = video.read()
+
+    if ret:
+        cv2.imshow('video', frame)
+        if cv2.waitKey(30) & 0xFF == ord('q'): # press 'q' to quit
+            break
+
+# release the video capture object and close all windows
+video.release()
+cv2.destroyAllWindows()
