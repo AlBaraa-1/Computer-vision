42800ff99b754d63630e53b5c0e5eb742dc9ffa3
AlBaraa-1 <666645@gmail.com>
2025-10-06 23:23:28 +0400

    Add image cropping functionality with OpenCV

---
+++ baby_steps/04_crop_img.py
@@ -0,0 +1,16 @@
+import cv2
+import os
+
+img_path = os.path.join('computer-vision/baby_steps', 'test.png')
+
+img = cv2.imread(img_path)
+
+# display the original image
+cv2.imshow('Original Image', img)
+
+# crop the image - (y1:y2, x1:x2)
+cropped_img = img[50:200, 200:350]
+
+cv2.imshow('Cropped Image', cropped_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
